fab0656b7412efa6ebef86e3998a3a07bb5c7dad
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-26 01:55:36 +0300

    redux updated

--- src/redux/phonebook/phonebook-types.js
+++
@@ -1,6 +0,0 @@
-// export default {
-//     ADD: 'phonebook/addContact',
-//     DELETE: 'phonebook/deleteContact',
-//     EDIT: 'phonebook/editContact',
-//     CHANGE_FILTER: 'phonebook/filterContact',
-// };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,8 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-// import logger from 'redux-logger';
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import phonebookReducer from "./phonebook/phonebook-reducer";
-
-const middleware = [
-    ...getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }),
-    // logger,
-];
-
-const contactsPersistConfig = {
-    key: 'contacts',
-    storage,
-    blacklist: ['filter'],
-};
+import { configureStore } from '@reduxjs/toolkit';
+import reducerContacts from './contacts/contactsReducer';
 
 const store = configureStore({
-    reducer: {
-        contacts: persistReducer(contactsPersistConfig, phonebookReducer),
-    },
-    middleware,
+    reducer: reducerContacts,
 });
 
-const persistor = persistStore(store);
-
-// eslint-disable-next-line
-export default { store, persistor };
+export default store;
